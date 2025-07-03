59e40e7269c7e15d652fea0b42959963bdd30f89
ivanmalov <ivan8malov@gmail.com>
2025-07-03 16:14:57 +0700

    update in config

--- dist/context.js
+++ dist/context.js
@@ -4,9 +4,13 @@ exports.resolveVisitorContext = resolveVisitorContext;
 const parse_1 = require("./parse");
 const currency_1 = require("geoinfo/currency");
 function resolveVisitorContext(input, opts = {}) {
-    var _a, _b, _c;
     const headers = input instanceof Request ? input.headers : input;
     const geo = (0, parse_1.parseGeo)(headers);
     // Assemble once and return
-    return Object.assign({ ip: (_a = headers.get('x-real-ip')) !== null && _a !== void 0 ? _a : null, currency: (_c = currency_1.currency[(_b = geo.countryCode) !== null && _b !== void 0 ? _b : '']) !== null && _c !== void 0 ? _c : null }, geo);
+    return {
+        ip: headers.get('x-real-ip') ?? null,
+        currency: currency_1.currency[geo.countryCode ?? ''] ?? null,
+        //headers: Object.fromEntries(headers),
+        ...geo
+    };
 }
--- dist/parse.js
+++ dist/parse.js
@@ -2,15 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseGeo = parseGeo;
 function parseGeo(headers) {
-    var _a, _b, _c, _d, _e, _f, _g, _h;
     return {
-        continentCode: (_a = headers.get('x-vercel-ip-continent')) !== null && _a !== void 0 ? _a : null,
-        countryCode: (_b = headers.get('x-vercel-ip-country')) !== null && _b !== void 0 ? _b : null,
-        regionCode: (_c = headers.get('x-vercel-ip-country-region')) !== null && _c !== void 0 ? _c : null,
-        city: (_d = headers.get('x-vercel-ip-city')) !== null && _d !== void 0 ? _d : null,
-        postalCode: (_e = headers.get('x-vercel-ip-postal-code')) !== null && _e !== void 0 ? _e : null,
-        longitude: parseFloat((_f = headers.get('x-vercel-ip-longitude')) !== null && _f !== void 0 ? _f : 'NaN'),
-        latitude: parseFloat((_g = headers.get('x-vercel-ip-latitude')) !== null && _g !== void 0 ? _g : 'NaN'),
-        timezone: (_h = headers.get('x-vercel-ip-timezone')) !== null && _h !== void 0 ? _h : null,
+        continentCode: headers.get('x-vercel-ip-continent') ?? null,
+        countryCode: headers.get('x-vercel-ip-country') ?? null,
+        regionCode: headers.get('x-vercel-ip-country-region') ?? null,
+        city: headers.get('x-vercel-ip-city') ?? null,
+        postalCode: headers.get('x-vercel-ip-postal-code') ?? null,
+        longitude: parseFloat(headers.get('x-vercel-ip-longitude') ?? 'NaN'),
+        latitude: parseFloat(headers.get('x-vercel-ip-latitude') ?? 'NaN'),
+        timezone: headers.get('x-vercel-ip-timezone') ?? null,
     };
 }
