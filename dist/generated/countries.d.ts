790fda376300e0a2cdd46d3b1f3083791d915fa6
ivanmalov <ivan8malov@gmail.com>
2025-07-07 12:49:03 +0700

    actualy update

--- dist/generated/countries.d.ts
+++ dist/generated/countries.d.ts
@@ -1,5 +1,6 @@
 export interface Country {
     name: string;
-    currency: string;
+    currency: string | null;
+    phone: string | null;
 }
 export declare const countries: Record<string, Country>;
